7f432f510e88049dfdf8a8d4d087a27f34a93345
churchill <churrgift@gmail.com>
2022-09-11 13:40:41 -0700

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a = 48, b = 97;
+while (a <= 57)
+{
+putchar(a);
+a++;
+}
+while(b<=102)
+{
+putchar(b);
+b++;
+}
+putchar('\n');
+return (0);
+}
