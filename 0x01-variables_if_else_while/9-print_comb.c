3231acd98ec63b5f56c32ff0e9dfb4aec2fc2365
churchill <churrgift@gmail.com>
2022-09-11 13:57:15 -0700

    update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a = 48;
+while (a <= 57)
+{
+putchar(a);
+if (a == 57)
+break;
+putchar(',');
+putchar(' ');
+a++;
+}
+return (0);
+}
