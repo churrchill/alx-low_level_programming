224968a4f4dd82488e993d8de04dc96bb139ed2e
churchill <churrgift@gmail.com>
2022-09-12 20:14:05 -0700

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,17 +6,18 @@
 *
 * Return: Always 0 (Success)
 */
-int main(void) {
-int a =48;
-int b =48;
+int main(void)
+{
+int a = 48;
+int b = 48;
 while (a < 57)
 {
 b = a + 1;
-while ( b <= 57)
+while (b <= 57)
 {
 putchar(a);
 putchar(b);
-if (a ==56 && b == 57)
+if (a == 56 && b == 57)
 break;
 putchar(',');
 putchar(' ');
