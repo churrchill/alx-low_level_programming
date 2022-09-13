9e634b49ce2271a9139197c589962641d8bf8b2d
churchill <churrgift@gmail.com>
2022-09-12 20:58:58 -0700

    update

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main()
+{
+int a = 48;
+int b;
+int c;
+int d; 
+while (a <= 57)
+{
+b = 48;
+while (b <= 57)
+{
+c = 48;
+while (c <= 57)
+{
+d = 48;
+while (d <= 57)
+{
+putchar(a);
+putchar(b);
+putchar(' ');
+putchar(c);
+putchar(d);
+if (a == 57 && b == 57 && c == 57 && d == 57)
+break;
+putchar(',');
+putchar(' ');
+d++;
+}
+c++;
+}
+b++;
+}
+a++;
+}  
+putchar('\n');
+return (0); 
+}
