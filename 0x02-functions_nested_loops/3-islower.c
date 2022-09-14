efc034257fcb4c84e58e74fbfbb74ceef5672bfa
churchill <churrgift@gmail.com>
2022-09-14 12:39:33 -0700

    new update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,11 @@
 *_islower - writes the character c to stdout
 *
 *@c for new number
+*
+*c number for paramerter
+*
 * Return: On success 1.
+*
 * On error, -1 is returned, and errno is set appropriately.
 */
 int _islower(int c)
