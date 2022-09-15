8a7e9944978b36ddf2d9fd9f203d2b577c1efbf4
churchill <churrgift@gmail.com>
2022-09-14 17:39:32 -0700

    new update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,25 +1,18 @@
 #include "main.h"
 /**
-* times_table - writes the character c to stdout
-*
-* no c is an ascii character
-*
-*c number for paramerter
-*
-* Return: On success 1.
-*
+* times_table - writes the character c to s
 * On error, -1 is returned, and errno is set appropriately.
 */
 void times_table(void)
 {
 int fact = 0;
-while ( fact < 10)
+while (fact < 10)
 {
 int i = 0;
 while (i < 10)
 {
 int res = fact * i;
-int res2 =fact * (i + 1);
+int res2 = fact * (i + 1);
 int tens = res / 10;
 int ones = res % 10;
 if (i == 9)
@@ -28,7 +21,7 @@ if (res >= 10)
 {
 _putchar(tens + '0');
 }
-_putchar(ones +'0');
+_putchar(ones + '0');
 }
 else  if (res >= 10){
 _putchar(tens + '0');
@@ -44,7 +37,7 @@ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 }
-else if (res2 >=10 && i !=9)
+else if (res2 >= 10 && i ! =9)
 {
 _putchar(',');
 _putchar(' ');
