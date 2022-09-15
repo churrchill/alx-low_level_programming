64f35f0a574db8b7c1fea510346367abb996e3ec
churchill <churrgift@gmail.com>
2022-09-15 06:39:51 -0700

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_last_digit - writes the character c to stdout
+*
+*@c: c is an ascii character
+*
+*c number for paramerter
+*
+* Return: On success 1.
+*
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
