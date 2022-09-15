df86674dbcde69c0a96d63f352be59237927e0d4
churchill <churrgift@gmail.com>
2022-09-14 17:21:55 -0700

    new update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,25 +19,27 @@ int i = 0;
 while (i < 10)
 {
 int res = fact * i;
+ int res2 =fact * (i+1);
  int tens = res / 10;
  int ones = res % 10;
  if (i == 9)
    {
-     if (res >10){
+     if (res >= 10){
        _putchar(tens+'0');}
 	 _putchar(ones +'0');
      
    }
- else  if (res >10){
+ else  if (res >= 10){
    _putchar(tens + '0');
    _putchar(ones +'0');
-     _putchar(',');
-   _putchar(' ');
  }
  else{
    _putchar(ones + '0');
-   _putchar(',');
-   _putchar(' ' );
+ }
+ if (res2 <10 && i!= 9)
+   {_putchar(' ');
+     _putchar(' ');}
+ else if (res2 >=10 && i !=9){
    _putchar(' ');}
 i++;
 }
