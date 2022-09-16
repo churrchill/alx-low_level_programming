1820fca12c0620a780a8accbb5bdc2e48fe13123
churchill <churrgift@gmail.com>
2022-09-16 06:54:18 -0700

    new update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,39 +1,24 @@
 #include <stdio.h>
+
 /**
 * main - check the code.
 *
 * Return: Always 0.
 */
+
 int main(void)
-  
 {
-  
-  int i;
-  
+int i;  
+printf("Infinite loop incoming :(\n");
+i = 0;
+/** while (i < 10)
 
-  
-  printf("Infinite loop incoming :(\n");
-  
+{
+putchar(i);
+}
 
-  
-  i = 0;
-  
 
-  
-  /** while (i < 10)
-    
-    {
-      
-      putchar(i);
-      
-    }
-  */
-
-  
-  printf("Infinite loop avoided! \\o/\n");
-  
-
-  
-  return (0);
-  
+*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
