854c8359401ad597d8deba3e07de2d3b992c121c
churchill <churrgift@gmail.com>
2022-09-16 07:50:18 -0700

    new update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,8 +2,7 @@
 
 
 /**
-* main - check the code.
-*
+* main - causes an infinite loop
 * Return: 0
 */
 
@@ -13,8 +12,8 @@ int main(void)
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
-/**
-* while (i < 10)
+/*
+while (i < 10)
 {
 putchar(i);
 }
