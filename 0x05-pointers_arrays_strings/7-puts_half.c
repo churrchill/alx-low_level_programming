43a8ba6aab368f72d63cd6cbc706376587d0a185
churchill <churrgift@gmail.com>
2022-09-19 21:14:17 -0700

    update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,15 +7,16 @@
 */
 void puts_half(char *str)
 {
-int i, j, max;
+int i, j;
 i = 0;
 while (str[i] != '\0')
 {
 i++;
 }
-max = i;
-j = max / 2;
-while (j <= max)
+j = i / 2;
+if (j % 2 != 0)
+j = (i + 1) / 2;
+while (j < i )
 {
 _putchar(str[j]);
 j++;
