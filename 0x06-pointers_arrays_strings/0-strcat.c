8263af291500712db0576cb9950d1ee709bb2027
churchill <churrgift@gmail.com>
2022-09-22 15:51:24 -0700

    new update

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _strcat - gets the lenght
+* @dest: a pointer parameter
+* @src: a second pointer
+* Return: void
+*/
+char *_strcat(char *dest, char *src)
+{
+int i = 0, n = 0;
+while (*(dest + n) != '\0')
+{
+n++;
+}
+while (*(src + i) != '\0')
+{
+*(dest + i + n) =* ( src + i);
+i++;
+}
+*(dest + i + n) = '\0';
+return dest;
+}
---
+++ 0x06-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* rev_string - gets the lenght
+* @s: a pointer parameter
+* Return: void
+*/
+void rev_string(char *s)
+{
+int i, max, half;
+char first, last;
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+max = i - 1;
+half = max / 2;
+while (half >= 0)
+{
+first = s[max - half];
+last = s[half];
+s[half] = first;
+s[max - half] = last;
+half--;
+}
+}
