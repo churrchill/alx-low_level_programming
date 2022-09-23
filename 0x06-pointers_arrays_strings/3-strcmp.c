925aac14e5dd4d0f688a3fcbd97de834def5f647
churchill <churrgift@gmail.com>
2022-09-22 18:01:29 -0700

    new update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,13 +2,13 @@
 #include "main.h"
 /**
 * _strcmp - function that copies a string
-* @s1: destination char
-* @s2: source char
-* Return: new string
+* @s1: s2
+* @s2: s1
+* Return: int
 */
 int _strcmp(char *s1, char *s2)
 {
-int i =0;
+int i = 0;
 while (s1[i] == s2[i])
 {
 if (s1[i] == '\0' && s2[i] =='\0')
