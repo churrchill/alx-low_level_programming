94dca195003d3e6185f7751432638fec8760407e
churchill <churrgift@gmail.com>
2022-09-28 15:56:21 -0700

    new update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,25 +2,27 @@
 #include "main.h"
 /**
 * _sqrt_recursion - gets the lenght
+* check - get the workdone
 * @n: a pointer parameter
-* @a: a inner function 
-* @b: a inner func param 
+* @a: a inner function
+* @b: a inner func param
 * Return: void
 */
-int check (int a ,int b)
+int check(int a, int b)
 {
 if (b / a == a && b % a == 0)
 return (a);
-else if ( a > (b / 2))
+else if (a > (b / 2))
 return (-1);
 else
-return (check(a+1, b));
+return (check(a + 1, b));
 }
 int _sqrt_recursion(int n)
 {
 int start = 1;
+int cop = n;
 if (n <= 0)
 return (-1);
 else
-return check(start, n);
+return (check(start, cop));
 }
