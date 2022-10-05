3f754cd89da58379f7c9880a7909cc2cf0a73e43
churchill <churrgift@gmail.com>
2022-10-04 18:11:48 -0700

    new update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,11 +10,11 @@
 char *str_concat(char *s1, char *s2)
 {
 char *s, t = '\0';
-int i = 0, j = 0, k, m=0;
+int i = 0, j = 0, k, m = 0;
 if (s1 == NULL)
 s1 = &t;
-else if (s2 == NULL )
-s2 = &t ;
+else if (s2 == NULL)
+s2 = &t;
 while (s1[i] != '\0')
 {
 i++;
@@ -24,7 +24,7 @@ while (s2[j] != '\0')
 j++;
 }
 k = i + j;
-s = malloc(k * sizeof(char));
+s = malloc(k *sizeof(char) + 1);
 if (s == NULL)
 return (NULL);
 while (m < i)
@@ -34,9 +34,9 @@ m++;
 }
 while (m < k)
 {
-s[m] = s2[m-i];
+s[m] = s2[m - i];
 m++;
 }
 s[m] = '\0';
-return s;
+return (s);
 }
