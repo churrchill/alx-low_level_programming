d10389a8d7771eb8b9dfd9e1fda6c2fb2d960863
churchill <churrgift@gmail.com>
2022-10-04 18:05:16 -0700

    new update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,16 +10,19 @@ char *_strdup(char *str)
 {
 int i, j = 0;
 char *s;
+if ( str == NULL)
+return (NULL);
 while (str[j] != '\0')
 {
 j++;
 }
-s = malloc(j *sizeof(char));
-if (str == NULL || s == NULL)
+s = malloc(j *sizeof(char) + 1);
+if (s == NULL)
 return (NULL);
 for (i = 0; i < j; i++)
 {
 s[i] = str[i];
 }
+s[i] = '\0';
 return (s);
 }
