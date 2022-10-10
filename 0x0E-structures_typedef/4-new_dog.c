d139a50f2ff9c7be07a48bc6c40d4ab1475a3c18
churchill <churrgift@gmail.com>
2022-10-10 15:30:15 -0700

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,11 +13,11 @@ char *arr;
 if (str == NULL)
 return (NULL);
 length = 0;
-while (*(str+length) != '\0')
+while (*(str + length) != '\0')
 {
 length++;
 }
-arr = (char*) malloc(length * sizeof(char) + 1);
+arr = malloc(length * sizeof(char) + 1);
 if (arr == NULL)
 return (NULL);
 for (i = 0; i < length; i++)
