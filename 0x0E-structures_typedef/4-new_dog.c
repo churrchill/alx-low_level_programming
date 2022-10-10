c83f8664bc46971e7811848db8866a237138144e
churchill <churrgift@gmail.com>
2022-10-10 15:09:32 -0700

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,18 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _strdup - returns a pointer to a newly allocated space in memory
- * @str: string
- * Return: pointer to newly allocated space in memory
- */
+* _strdup - returns a pointer to a newly allocated space in memory
+* @str: string
+* Return: pointer to newly allocated space in memory
+*/
 char *_strdup(char *str)
 {
 int length, i;
 char *arr;
 if (str == NULL)
-{
 return (NULL);
-}
 length = 0;
 while (*(str + length) != '\0')
 {
@@ -30,12 +28,12 @@ arr[i] = '\0';
 return (arr);
 }
 /**
- * new_dog - creates a new dog
- * @name: doggo's name
- * @age: doggo's age
- * @owner: doggo's owner
- * Return: new_dog (dog_t)
- */
+* new_dog - creates a new dog
+* @name: doggo's name
+* @age: doggo's age
+* @owner: doggo's owner
+* Return: new_dog (dog_t)
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *doggo;
