1a6e4452a09f38e53f693f72f386ad25c6600d10
churchill <churrgift@gmail.com>
2022-10-11 19:02:12 -0700

    update

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,4 +1,4 @@
 #ifndef FUNCTION_LIKE_MICRO_H
 #define FUNCTION_LIKE_MICRO_H
-#define ABS(X) ((X) < (0) ? (X * -1) : (X))
+#define ABS(X) ((X) < 0 ? (X * -1) : (X))
 #endif /* !FUNCTION_LIKE_MICRO_H */
